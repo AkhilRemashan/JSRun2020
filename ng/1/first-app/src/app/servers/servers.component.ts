aff0b9d3227abc8e60f56663f38a6b639459ef28
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-04-11 03:14:46 -0700

    compo

---
+++ ng/1/first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: 'src/app/servers/servers.component.ts'
+})
+
+export class ServersComponent{
+
+}
