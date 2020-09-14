2d66e92403bff31a15ae31456195d222b0b23a8d
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-09-14 21:27:25 +0530

    prop-revise

--- react/base-rt/src/components/TodoItem.js
+++ react/base-rt/src/components/TodoItem.js
@@ -3,6 +3,7 @@ import React from 'react'
 function TodoItem(){
     return(
         <div className="todo-item">
+            <img src="" />
             <input type="checkbox" name="input-1" value=" " />
             <label for="input-1"> I have a bike, purrr</label>
         </div>
