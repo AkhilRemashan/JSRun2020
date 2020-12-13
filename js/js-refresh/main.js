52894d105093d599582ba96dc6a4d22a070c6ef3
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-12-14 05:10:36 +0530

    jsr-1

---
+++ js/js-refresh/main.js
@@ -0,0 +1,17 @@
+
+// variables
+
+var drink = "smoothie";
+console.log(drink);
+
+
+
+
+
+// 
+
+ var age = prompt("what's your age?");
+
+ // 
+
+document.getElementById('someText1').innerHTML = age;
