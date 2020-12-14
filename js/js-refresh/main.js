a270dfe79de46d83e01584afef0420981f858405
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2020-12-15 04:59:21 +0530

    fun-part

--- js/js-refresh/main.js
+++ js/js-refresh/main.js
@@ -1,17 +1,50 @@
 
-// variables
+// ####### variables
 
-var drink = "smoothie";
-console.log(drink);
+/* var drink = "smoothie";
+console.log(drink); */
+
+/* var age = prompt("what's your age?");
+
+ document.getElementById('someText1').innerHTML = age; */
 
 
+// ######## function and call
+
+/* function fun() {
+    console.log("have fun!");
+}
+
+fun(); */
+
+// ######## function returns value
+
+/* function helloName() {
+    var name = prompt ("whats your name?");
+    var result = "Hello " + name;
+    console.log(result);
+}
+
+helloName(); */
+
+// ######### function with arguments
+
+/* function addNum(num1, num2) {
+    var result = num1 + num2;
+    console.log(result);
+}
+
+addNum(19, 7); */
 
 
+// ######### while loops
 
-// 
+/* var num = 0;
 
- var age = prompt("what's your age?");
+while(num < 100) {
+    num += 1;
+    console.log(num);
+} */
 
- // 
 
-document.getElementById('someText1').innerHTML = age;
+// ######### for loops
