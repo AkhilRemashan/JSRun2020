f1363b9c9f2ddea938686a2251371a3e7fc8fe55
Akhil Remashan <34641800+AkhilRemashan@users.noreply.github.com>
2021-01-02 19:07:22 +0530

    skel-build1

---
+++ mobile-app/vs_setup.js
@@ -0,0 +1,25 @@
+
+const commands = {
+
+    expo: "npm i -g expo-cli",
+
+}
+
+const plugins = {
+
+    vscode_plugins: [
+        "react native tools - microsoft",
+        "react native/react/redux snippets - equimper",
+        "prettier - esben",
+        "material icon themes - philip"
+    ],
+    
+}
+
+const setting = {
+
+    vscode_settings: [
+        "file > preferences > setting > format on save",
+    ],
+    
+}
